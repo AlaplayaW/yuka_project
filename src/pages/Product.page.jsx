357f63fdf1ac4a68d028c0e578d0faa506f24255
Cloe Truel <cloe.truel@gmail.com>
2019-04-23 12:01:33 +0200

    put right picto in the product page and add an ooops picto for product without nutriscore

--- src/pages/Product.page.jsx
+++ src/pages/Product.page.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 
-import Fetch from '../Fetch';
-import Product from '../components/Product';
+import Fetch from "../Fetch";
+import Product from "../components/Product";
 
 // Le nom du pattern : 'render-props' ---- cf. Inversion de la dépendance >> COMMENT TO KEEP
-export default function ProductPage({ match }){
+export default function ProductPage({ match }) {
 	return (
-		<Fetch 
-			requestUrl={`https://fr.openfoodfacts.org/api/v0/produit/${match.params.barcode}`} 
+		<Fetch
+			requestUrl={`https://fr.openfoodfacts.org/api/v0/produit/${
+				match.params.barcode
+			}`}
 			renderError={() => "Oups.... une erreur est survenue"}
-			renderLoading={() => "Chargement du produit en cours ...."} 
-			renderSuccess={({product}) => {
-				debugger;
-				return <Product 
-				name={product.product_name_fr} 
-				brand={product.brands}
-				imageUrl={product.image_front_small_url}
-				novagroup={product.nova_group} 
-				sugars_value={product.nutriments.sugars_value}
-				sugars_unit={product.nutriments.sugars_unit}
-				energyV={product.nutriments.energy_value}
-				energyU={product.nutriments.energy_unit}
-				fatV={product.nutriments.fat_value}
-				fatU={product.nutriments.fat_unit}
-				saturedFatV={product["nutriments"]["saturated-fat_value"]}
-				saturedFatU={product["nutriments"]["saturated-fat_unit"]}
-				saltV={product.nutriments.salt_value}
-				saltU={product.nutriments.salt_unit}
-				/>
-		}} />
-	)
+			renderLoading={() => "Chargement du produit en cours ...."}
+			renderSuccess={({ product }) => {
+				return (
+					<Product
+						name={product.product_name_fr}
+						brand={product.brands}
+						imageUrl={product.image_front_small_url}
+						novagroup={product.nova_group}
+						// nutriscore={product["nutriments"]["nutrition-score-fr"]}
+						nutriscore={product.nutrition_grades}
+						sugars_value={product.nutriments.sugars_value}
+						sugars_unit={product.nutriments.sugars_unit}
+						energyV={product.nutriments.energy_value}
+						energyU={product.nutriments.energy_unit}
+						fatV={product.nutriments.fat_value}
+						fatU={product.nutriments.fat_unit}
+						saturedFatV={product["nutriments"]["saturated-fat_value"]}
+						saturedFatU={product["nutriments"]["saturated-fat_unit"]}
+						saltV={product.nutriments.salt_value}
+						saltU={product.nutriments.salt_unit}
+					/>
+				);
+			}}
+		/>
+	);
 }
