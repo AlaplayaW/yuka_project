177e2b631d0876795cd3b3d201e2e388fb575c58
Jean-Baptiste <gervais.jbaptiste@gmail.com>
2019-04-26 12:25:33 +0200

    search bar add

--- src/pages/Error.page.jsx
+++ src/pages/Error.page.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import SearchInput from "../components/SearchInput";
+import SearchButton from "../components/SearchButton";
+import {Redirect} from "react-router-dom";
 
-const Error = () => {
+import Qapp from "../quagga/App"
+
+
+export default class Error extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      barCode: "",
+    };
+  }
+
+  render() {
+    const { inputValue, barCode } = this.state;
+  if (barCode) {return <Redirect to={`/product/${barCode}`}/>}
+
   return (
     <div>
-      <p>Error: Path does not exist !</p>
+      <p style={{textAlign: 'center', fontFamily: 'Patrick Hand SC', fontSize: '2em'}}>OOOPS ! Ce produit n'a pas été trouvé !</p>
+      <p style={{textAlign: 'center', fontFamily: 'Patrick Hand SC', fontSize: '2em'}}>Retente ta chance juste en dessous !</p>
+    <SearchInput onChange={inputValue => this.setState({ inputValue })} value={inputValue}/>
+    <SearchButton barcode={inputValue} />
+    <Qapp />
+
     </div>
   )
-};
-
-export default Error;
+  }
+}
