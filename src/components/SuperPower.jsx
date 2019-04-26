4ff46648869d15d11df184323518f66a3fb97006
Perrine Martyris <perrinemartyris@gmail.com>
2019-04-26 13:18:58 +0200

    new cards placement

--- src/components/SuperPower.jsx
+++ src/components/SuperPower.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import styles from "./SuperPower.module.css";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 const SuperPower = props => {
 	return (
-		<>
+		<div className="col-3 mx-2">
 			<Card
-				className="bg-transparent mx-1 d-none d-lg-block"
+				className="bg-transparent m-3 d-none d-lg-flex "
 			>
-				<CardImg src={props.image} alt="Power" />
-				<CardBody>
+				<CardImg src={props.image} alt="Power" className={`${styles.image} mx-auto`} />
+				<CardBody className="p-1">
 					<CardTitle className="p-1 text-center">{props.power}</CardTitle>
 					<CardText>{props.description}</CardText>
 				</CardBody>
 			</Card>
-		</>
+		</div>
 	);
 };
 
