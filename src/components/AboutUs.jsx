a7946ea1cc26f66bcbdae92c2325c19a53fe52fd
Perrine Martyris <perrinemartyris@gmail.com>
2019-04-29 10:43:46 +0200

    resolve the aboutUs problems

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,52 +1,20 @@
 import React, { Component } from "react";
 import { Card, Row, Col, CardTitle, CardImg } from "reactstrap";
 
-import MemberTeam from "./MemberTeam";
 import Jim from "../images/jim.png";
 import JB from "../images/jb.png";
 import Perrine from "../images/perrine.png";
 import Cloe from "../images/cloe.png";
-import styles from "./Member.module.css";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import styles from "./AboutUs.module.css";
 
-class AboutUs extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			members: [
-				{
-					name: "Perrine",
-					presentation:
-						"La super héroïne du scan !! Elle accumule les supers pouvoirs grâce à ses scans ",
-					image: Perrine
-				},
-				{
-					name: "Jim",
-					presentation:
-						"Sauveur de tortues, Jim mène un combat quotidien pour rendre le monde meilleur !! A bat les produits",
-					image: Jim
-				},
-				{
-					name: "Jean-Baptiste",
-					presentation:
-						"Le héro de l'info, il cherche et déniche toutes les infos produits pour les mettre en ligne!! Rien ne lui échappe",
-					image: JB
-				},
 
-				{
-					name: "Cloé",
-					presentation: " lalalala",
-					image: Cloe
-				}
-			]
-		};
-	}
-	render() {
-		return (
-			<Layout>
-			<div className="container-fluid">
-				<Row className="d-flex justify-content-center m-5">
-					{/* <p className={styles.titleMemberPage}> LA TEAM YUKIDS </p> */}
+export default function AboutUs() {
+	return (
+		<Layout>
+			<div className={`${styles.bgImg} container-fluid`}>
+				<Row className="d-flex justify-content-center">
+					<p className={styles.titleMemberPage}> LA TEAM YUKIDS </p>
 				</Row>
 				<Row className="d-flex justify-content-center">
 					<Col lg={{ size: 3, offset: 1 }} className="m-3">
@@ -91,18 +59,6 @@ class AboutUs extends Component {
 					</Col>
 				</Row>
 			</div>
-			</Layout>
-
-			// {/*
-			// 	<div className="container-fluid">
-			// 		<Row className="d-flex justify-content-center">
-			// 			{this.state.members.map((team, index) => {
-			// 				return <MemberTeam {...team} key={index} />;
-			// 			})}
-			// 		</Row>
-			// 	</div> */}
-		);
-	}
+		</Layout>
+	);
 }
-
-export default AboutUs;
