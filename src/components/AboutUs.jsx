d018f1daa40e107481b8e4748ba14e6a52053544
Cloe Truel <cloe.truel@gmail.com>
2019-04-30 16:32:17 +0200

    css page a propos ok

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -14,17 +14,16 @@ export default function AboutUs() {
 			className={`${styles.bgImg} container-fluid`}
 			style={{ minHeight: "84vh" }}
 		>
-			<Row className="d-flex justify-content-center">
+			<Row className="d-flex justify-content-center pt-4">
 				<p className={styles.titleMemberPage}> LA TEAM YUKIDS </p>
 			</Row>
 			<Row className="d-flex justify-content-center">
-				<Col lg={{ size: 6 }}>
-					<p>
+				<Col className="p-2" lg={{ size: 6 }}>
+					<p className={styles.prezMember}>
 						La team Yukids c'est le groupement de 4 supers kids (qui ne
 						grandiront jamais) et qui s'intéressent de près aux pouvoirs des
-						aliments. Chercher, lire, scanner, coder sont les passions de ces 4
-						supers qui vous embarquent avec eux dans leur merveilleuse aventure
-						: sauver le monde !!
+						aliments. Ces 4 supers héros qui vous embarquent avec eux dans leur
+						merveilleuse aventure : SAUVER LE MONDE !!
 					</p>
 				</Col>
 			</Row>
@@ -34,7 +33,7 @@ export default function AboutUs() {
 						<CardImg src={Perrine} alt="lalala" />
 						<p>
 							La super héroïne du scan !! Elle accumule les supers pouvoirs
-							grâce à ses scans
+							grâce à ses scans de bons produits.
 						</p>
 						<CardTitle className={styles.CardTitle}> PERRINE </CardTitle>
 					</Card>
@@ -44,7 +43,7 @@ export default function AboutUs() {
 						<CardImg src={Jim} alt="lalala" />
 						<p>
 							Sauveur de tortues, Jim mène un combat quotidien pour rendre le
-							monde meilleur !! A bat les produits
+							monde meilleur !!
 						</p>
 						<CardTitle className={styles.CardTitle}> JIM </CardTitle>
 					</Card>
@@ -55,9 +54,8 @@ export default function AboutUs() {
 					<Card className={styles.hovereffect} body outline color="warning">
 						<CardImg src={JB} alt="lalala" />
 						<p>
-							{" "}
 							Le héro de l'info, il cherche et déniche toutes les infos produits
-							pour les mettre en ligne!! Rien ne lui échappe{" "}
+							pour les mettre en ligne!! Rien ne lui échappe...
 						</p>
 						<CardTitle className={styles.CardTitle}>JEAN-BAPTISTE</CardTitle>
 					</Card>
@@ -65,7 +63,10 @@ export default function AboutUs() {
 				<Col lg={{ size: 3, offset: 1 }} className="m-3">
 					<Card className={styles.hovereffect} body outline color="warning">
 						<CardImg src={Cloe} alt="lalala" />
-						<p> lalala </p>
+						<p>
+							La super héroïne du scan !! Elle accumule les supers pouvoirs
+							grâce à ses scans de bons produits.
+						</p>
 						<CardTitle className={styles.CardTitle}> CLOÉ </CardTitle>
 					</Card>
 				</Col>
