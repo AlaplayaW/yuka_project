3db292f0c7e5e4189c3a7451b44dd3ebf9619870
Jim Decocq <jim.decocq@gmail.com>
2019-05-01 18:50:17 +0200

    switch on error page working with a Quagga clone component

--- src/pages/Error.page.jsx
+++ src/pages/Error.page.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
 import SearchInput from "../components/SearchInput";
 import SearchButton from "../components/SearchButton";
-
-import {Redirect} from "react-router-dom";
-
+import { Redirect } from "react-router-dom";
 import Layout from "../components/Layout";
-
 import SwitchButton from "../components/SwitchButton";
+import styles from "./Error.page.module.css";
 
 export default class Error extends React.Component {
 	constructor(props) {
@@ -18,48 +15,38 @@ export default class Error extends React.Component {
 		};
 	}
 
+	render() {
+		const { inputValue, barCode } = this.state;
+		if (barCode) {
+			return <Redirect to={`/product/${barCode}`} />;
+		}
 
-render() {
-	const { inputValue, barCode } = this.state;
-	if (barCode) {
-		return <Redirect to={`/product/${barCode}`} />;
+		return (
+			<Layout>
+				<div className={styles.block}
+				>
+					<div>
+						<p className={styles.textLaptop}>
+							OOOPS ! Le scan du produit n'a pas marché...
+						</p>
+						<p className={styles.textLaptop}>
+							Tu peux rentrer son code à la main ou réessayer de scanner le code barre en appuyant sur ce bouton
+						</p>
+					</div>
+
+							<div >
+								<SwitchButton
+									onChange={inputValue => this.setState({ inputValue })}
+									value={inputValue}
+									barcode={inputValue}
+								/>
+							</div>
+						
+					
+					</div>
+		
+				
+			</Layout>
+		);
 	}
-
-	return (
-		<Layout>	
-		<div style={{ 
-			textAlign: "center", 
-			textShadow: "3px 3px 3px black",
-			color: '#FF6319',
-			fontSize: '1.5em',
-			height: '80vh',
-			backgroundColor: '#FEDE4F',
-			}}>
-				<p
-					style={{
-						textAlign: "center",
-						fontFamily: "Patrick Hand SC",
-						fontSize: "2em"
-					}}
-				>
-					OOOPS ! Ce produit n'a pas été trouvé
-				</p>
-				<p
-					style={{
-						textAlign: "center",
-						fontFamily: "Patrick Hand SC",
-						fontSize: "2em"
-					}}
-				>
-					Retente ta chance juste en dessous !
-				</p>
-				<SwitchButton	
-				onChange={inputValue => this.setState({ inputValue })}
-				value={inputValue}
-				barcode={inputValue}
-				/>
-			</div>
-		</Layout>
-	);
-}
 }
